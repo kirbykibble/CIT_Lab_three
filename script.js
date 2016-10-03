ed16a0cfec027b80aeac4334fcf0cb3d392156ba
Dylan Chew <Dylan Chew>
2016-10-03 12:01:39 -0700

    increased the bees

--- script.js
+++ script.js
@@ -4,35 +4,25 @@ function buzz() {
     var beeBut = document.getElementById("beebutton");
     beeBut.innerHTML = "DEACTIVATE THE BEES"
     beeBut.setAttribute("onclick", "stop();")
+    beeBut.style.backgroundSize = "500px 288px";
+    document.getElementById("bees").style.animationDuration = "0.5s";
     
     setInterval(function() {
-        var rando = Math.floor((Math.random() * 5) + 1);
-        if (rando == 1) {
-            beeBut.style.marginLeft = "100px";
-        }
-        else if (rando == 2) {
-            beeBut.style.marginTop = "200px";
-        }
-        else if (rando == 3) {
-            beeBut.style.marginLeft = "20px";
-            beeBut.style.marginTop = "500px";
-        }
-        else if (rando == 4) {
-            beeBut.style.marginLeft = "-100px";
-            beeBut.style.marginTop = "-20px"
-        }
-        else if (rando == 5) {
-            beeBut.style.marginLeft = "1000px";
-            beeBut.style.marginTop = "-300px";
-        }
-        else {
-            alert("errors")
-        }
-        
-        
-        
+        beeBut.style.marginLeft = ((Math.round(Math.random()) * 1000) - 500) + "px";
+        beeBut.style.marginTop = ((Math.round(Math.random()) * 1000) - 500) + "px";
     }, 50);
 }
 function stop() {
     alert("YOU CANT DEACTIVATE THE BEES")
+    var bee = document.createElement("div");
+    document.body.appendChild(bee);
+    bee.setAttribute("ID", "beebutton")
+    bee.setAttribute("onclick", "stop();")
+    bee.innerHTML = "DEACTIVATE THE BEES"
+    bee.style.backgroundSize = "500px 288px";
+    
+    setInterval(function() {
+        bee.style.marginLeft = ((Math.round(Math.random()) * 1000) - 500) + "px";
+        bee.style.marginTop = ((Math.round(Math.random()) * 1000) - 500) + "px";
+    }, 50);
 }
